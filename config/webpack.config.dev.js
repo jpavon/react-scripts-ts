55c0ab604b01fa81129eae6a1a91d1196c04ba0c
Julio Pavón <julio.pavon@oakwood.se>
2018-03-22 17:06:04 +0100

    fix ,

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -258,7 +258,7 @@ module.exports = {
                 }
               }
             ],
-          },,
+          },
           // "file" loader makes sure those assets get served by WebpackDevServer.
           // When you `import` an asset, you get its (virtual) filename.
           // In production, they would get copied to the `build` folder.
