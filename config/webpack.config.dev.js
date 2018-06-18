853145477f3a23cbdfe1e86108c0123cfa00b23a
Julio Pavón <julio@jpavon.com>
2018-06-18 13:14:59 +0200

    change autoprefixer config

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -211,13 +211,7 @@ module.exports = {
                   ident: 'postcss',
                   plugins: () => [
                     require('postcss-flexbugs-fixes'),
-                    autoprefixer({
-                      browsers: [
-                        '>1%',
-                        'last 4 versions',
-                        'Firefox ESR',
-                        'not ie < 9', // React doesn't support IE8 anyway
-                      ],
+                    require('autoprefixer')({
                       flexbox: 'no-2009',
                     }),
                   ],
