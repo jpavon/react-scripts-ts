fc4d94459bc90fbeef656ad2104cc3904e69b983
Julio Pavón <julio@jpavon.com>
2018-06-18 19:41:37 +0200

    update webpack version

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -329,7 +329,7 @@ module.exports = {
       tsconfig: paths.appTsConfig,
       tslint: paths.appTsLint,
     }),
-    new HardSourceWebpackPlugin()
+    // new HardSourceWebpackPlugin()
   ],
   // Some libraries import Node modules but don't use them in the browser.
   // Tell Webpack to provide empty mocks for them so importing them works.
