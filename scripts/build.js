07544f05d3f066359651cad0da6f5c8d9b6c0429
Julio Pavón <julio@jpavon.com>
2018-06-19 18:17:14 +0200

    remove verifyPackageTree

--- scripts/build.js
+++ scripts/build.js
@@ -23,10 +23,10 @@ process.on('unhandledRejection', err => {
 require('../config/env');
 // @remove-on-eject-begin
 // Do the preflight check (only happens before eject).
-const verifyPackageTree = require('./utils/verifyPackageTree');
-if (process.env.SKIP_PREFLIGHT_CHECK !== 'true') {
-  verifyPackageTree();
-}
+// const verifyPackageTree = require('./utils/verifyPackageTree');
+// if (process.env.SKIP_PREFLIGHT_CHECK !== 'true') {
+//   verifyPackageTree();
+// }
 // @remove-on-eject-end
 
 const path = require('path');
