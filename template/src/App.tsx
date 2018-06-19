64fe913cb6408f89894fb3491c3595476bbb32b6
Julio Pavón <julio@jpavon.com>
2018-06-19 18:11:53 +0200

    update template files

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -1,14 +1,14 @@
 import * as React from 'react';
 import './App.css';
 
-const logo = require('./logo.svg');
+import LogoSvg from 'logo.svg';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <LogoSvg />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
