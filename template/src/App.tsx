e7ce98f5b552f67361ce6ab3ce3ba4b187b18277
Julio Pavón <julio@jpavon.com>
2018-06-20 12:24:26 +0200

    add public to render method

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -4,7 +4,7 @@ import './App.css';
 import LogoSvg from 'logo.svg';
 
 class App extends React.Component {
-  render() {
+  public render() {
     return (
       <div className="App">
         <header className="App-header">
