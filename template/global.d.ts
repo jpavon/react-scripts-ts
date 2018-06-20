a58e64742b741f67bcb034d95c746aa5c89f4f86
Julio Pavón <julio@jpavon.com>
2018-06-20 12:16:21 +0200

    add d.ts file to template

---
+++ template/global.d.ts
@@ -0,0 +1,15 @@
+declare module '*.gif'
+declare module '*.jpg'
+declare module '*.jpeg'
+declare module '*.png'
+declare module '*.svg'
+
+// // Uncomment this if you use sass/css modules
+// declare module '*.scss' {
+//     const content: any
+//     export default content
+// }
+// declare module '*.css' {
+//     const content: any
+//     export default content
+// }
