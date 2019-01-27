c0e523da5d41fa9145c1019d42533be4ffdbdbfb
Cyprien Taque <Cyprien.taque@gmail.com>
2019-01-27 20:18:58 +0100

    extra config file is optional

--- config/webpack.config.dev.extend.js
+++
@@ -1,4 +0,0 @@
-const paths = require('./paths');
-const baseConfig = require('./webpack.config.dev');
-const extendConfigFn = require(paths.appPath + '/webpack.config.extend.js')
-module.exports = extendConfigFn(baseConfig, process.env.NODE_ENV, { paths })
--- config/webpack.config.prod.extend.js
+++
@@ -1,5 +0,0 @@
-const paths = require('./paths');
-const baseConfig = require('./webpack.config.prod');
-const extendConfigFn = require(paths.appPath + '/webpack.config.extend.js')
-
-module.exports = extendConfigFn(baseConfig, process.env.NODE_ENV, { paths })
